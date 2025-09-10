8d8d48a0cb6a8ab03801bd61e3633ec4222b7694
kavyamehta987 <mehtakavya709@gmail.com>
2025-09-11 00:36:23 +0530

    Add files via upload

---
+++ printing name n times.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ int i,n;
+ printf("Enter any number:");
+ scanf("%d",&n);
+ printf("Printing my name %d times\n",n);
+ for(i=1;i<=n;i++)
+ {
+  printf("Kavya Mehta\n");
+ }
+ return 0;
+}
