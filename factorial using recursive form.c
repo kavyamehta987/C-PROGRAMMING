7047fb15e9d6dc49ad36b8aa149aa654688dee10
kavyamehta987 <mehtakavya709@gmail.com>
2025-11-07 12:23:44 +0530

    Add files via upload

---
+++ factorial using recursive form.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// Function to calculate factorial recursively
+int factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1; // Base case
+    else
+        return n * factorial(n - 1); // Recursive call
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num < 0)
+        printf("Factorial is not defined for negative numbers.\n");
+    else
+        printf("Factorial of %d is: %d\n", num, factorial(num));
+
+    return 0;
+}
